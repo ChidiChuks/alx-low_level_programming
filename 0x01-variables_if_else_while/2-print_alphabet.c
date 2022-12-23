78c160a1694ba4aaeb5d3986bd2ccbccd7ca9b24
Chidiebere Chukwuma <chukschiboy@gmail.com>
2022-12-23 07:25:30 -0800

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+
+	char a;
+
+	a = 'c';
+
+	while
+		(a <= 'u')
+		{
+			putchar(a);
+			a++;
+		}
+	putchar('\n');
+	return (0);
+
+}
