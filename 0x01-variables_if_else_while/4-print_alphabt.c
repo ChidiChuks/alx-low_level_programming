a503593c444901599398b64038d5abc47839f671
Chidiebere Chukwuma <chukschiboy@gmail.com>
2022-12-23 08:10:39 -0800

    removing some alphabts

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char a;
+
+	a = 'a';
+	while
+		(a <= 'z')
+		{
+			if ((a != 'q' && a != 'e') && a <= 'z')
+				putchar(a);
+			a++;
+		}
+	putchar('\n');
+	return (0);
+}
