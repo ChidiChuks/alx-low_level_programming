fc71d01419a3d96e38be43c9a57640cc11d2d833
Chidiebere Chukwuma <chukschiboy@gmail.com>
2023-01-17 04:14:26 -0800

    debugging

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,19 +1,23 @@
 #include <unistd.h>
 
+
+
 /**
  *
- * *_putchar - writes the character c to stdout
+ *  * _putchar - writes the character c to stdout
  *
- * *@c: The characters to print
+ *   * @c: The character to print
  *
- * *
+ *    *
  *
- * * Return: On success 1.
+ *     * Return: On success 1.
  *
- * * On error, -1 is returned, and error is set appropriately
- */
+ *      * On error, -1 is returned, and errno is set appropriately*/
 
 int _putchar(char c)
+
 {
-	return (write(1, &c, 1));
+
+		return (write(1, &c, 1));
+
 }
