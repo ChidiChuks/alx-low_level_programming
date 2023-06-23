d17bb98135a4481b647032f2a8b04e38a739a833
Chidiebere Chukwuma <chukschiboy@gmail.com>
2023-06-23 05:16:29 -0700

    string length update

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,17 +1,41 @@
 #include "main.h"
 
 /**
- * _strlen - return the length of a string
- * @s: character to check
- * Description: this will return the length of a string
- * Return: Always 0 (success)
+ * _strlen - Convert a string to an integer
+ * @s: The pointer to convert
+ *
+ * Return: An integer
  */
 
 int _strlen(char *s)
 {
-	int a = 0;
+	int c = 0;
+	unsigned int ni = 0;
+	int min = 1;
+	int isi = 0;
 
-	for (; *s++;)
-		a++;
-	return (a);
+	while (s[c])
+	{
+		if (s[c] == 45)
+		{
+			min *= -1;
+		}
+
+		while (s[c] >= 48 && s[c] <= 57)
+		{
+			isi = 1;
+			ni = (ni * 10) + (s[c] - '0');
+			c++;
+		}
+
+		if (isi == 1)
+		{
+			break;
+		}
+
+		c++;
+	}
+
+	ni *= min;
+	return (ni);
 }
