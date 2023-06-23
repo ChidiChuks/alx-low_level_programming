a1291e13108a09a9908d6e17ba4afd081be784e1
Chidiebere Chukwuma <chukschiboy@gmail.com>
2023-06-23 07:22:08 -0700

    compiling the script of 101-md5_gm.c using a C compiler

--- 0x18-dynamic_libraries/101-md5_gm.c
+++ 0x18-dynamic_libraries/101-md5_gm.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+#include <string.h>
 
-void __attribute__((constructor)) init(void) {
-    int fd;
-    char jackpot[] = "9 8 10 24 75 9\n";
-    
-    fd = open("/tmp/gm_numbers", O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-    if (fd != -1) {
-        write(fd, jackpot, sizeof(jackpot) - 1);
-        close(fd);
+int main(void)
+{
+    FILE *file;
+    char cmd[100];
+
+    file = fopen("/tmp/gm_numbers", "w");
+    if (file == NULL) {
+        printf("Failed to create /tmp/gm_numbers file\n");
+        return 1;
     }
+
+    fprintf(file, "9\n8\n10\n24\n75\n9\n");
+    fclose(file);
+
+    strcpy(cmd, "chmod 777 /tmp/gm_numbers");
+    system(cmd);
+
+    return 0;
 }
