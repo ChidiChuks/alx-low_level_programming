c11346032889da28282988f4059fe4bebb5d7757
Chidiebere Chukwuma <chukschiboy@gmail.com>
2023-06-23 07:33:07 -0700

    modified script, the /tmp/gm_numbers file

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-cp /tmp/gm_numbers ./gm
-chmod +x ./gm
+
+# Create the /tmp/gm_numbers file
+echo -e "9\n8\n10\n24\n75\n9" > /tmp/gm_numbers
+
+# Set the appropriate permissions for the file
+chmod 777 /tmp/gm_numbers
+
+# Run the Giga Millions program with the specified numbers
+./gm 9 8 10 24 75 9
