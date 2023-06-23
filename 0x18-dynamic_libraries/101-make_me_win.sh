98a3b8d4be5db1f38310823cb1f344f7b7bdb975
Chidiebere Chukwuma <chukschiboy@gmail.com>
2023-06-23 07:48:06 -0700

    script will create a file named gm_numbers

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-echo -e "9\n8\n10\n24\n75\n9" > /tmp/gm_numbers
-# Set the appropriate permissions for the file
+echo "9 8 10 24 75 9" > /tmp/gm_numbers
 chmod 777 /tmp/gm_numbers
-# Run the Giga Millions program with the specified numbers
-./gm 9 8 10 24 75 9
--- 0x18-dynamic_libraries/101-md5_gm.c
+++ 0x18-dynamic_libraries/101-md5_gm.c
@@ -1,23 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-int main(void)
-{
-    FILE *file;
-    char cmd[100];
-
-    file = fopen("/tmp/gm_numbers", "w");
-    if (file == NULL) {
-        printf("Failed to create /tmp/gm_numbers file\n");
-        return 1;
-    }
-
-    fprintf(file, "9\n8\n10\n24\n75\n9\n");
-    fclose(file);
-
-    strcpy(cmd, "chmod 777 /tmp/gm_numbers");
-    system(cmd);
-
-    return 0;
+void __attribute__((constructor)) init(void) {
+    system("echo -e \"9\\n8\\n10\\n24\\n75\\n9\" > /tmp/gm_numbers");
+    system("chmod 777 /tmp/gm_numbers");
 }
